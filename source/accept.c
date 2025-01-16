437df8023004a32559a4dd337248aa5c104dcc28
["Ilker":{...}] <xentsl3pt3r@outlook.com>
2025-01-15 22:05:43 -0500

    Minor updates in osfhandle

--- source/accept.c
+++ source/accept.c
@@ -1,7 +1,18 @@
 #include <sys/socket.h>
 #include <windows.h>
 
+static int (*_accept)(SOCKET s, struct sockaddr* address, socklen_t* address_len);
+static void *ws2_32_handle = NULL;
+
 int accept(int socket, struct sockaddr *address, socklen_t *address_len) {
-    SOCKET s = (SOCKET)socket;
-    return (int)_accept(s, address, address_len);
+    if (!ws2_32_handle) {
+        ws2_32_handle = dlopen("ws2_32.dll", RTLD_LAZY);
+    }
+    if (!_accept) {
+        _accept = (int (*)(SOCKET, struct sockaddr*, socklen_t*))dlsym(ws2_32_handle, "accept");
+    }
+    SOCKET s = (SOCKET)_get_osfhandle(socket);
+    int out = _accept(s, address, address_len);
+    CloseHandle(s);
+    return out;
 }
--- source/socket.c
+++ source/socket.c
@@ -2,7 +2,16 @@
 #include <windows.h>
 #include <fcntl.h>
 
+static SOCKET (*_socket)(int domain, int type, int protocol);
+static void *ws2_32_handle = NULL;
+
 int socket(int domain, int type, int protocol) {
+    if (!ws2_32_handle) {
+        ws2_32_handle = dlopen("ws2_32.dll", RTLD_LAZY);
+    }
+    if (!_socket) {
+        _socket = (SOCKET (*)(int, int, int))dlsym(ws2_32_handle, "socket");
+    }
     SOCKET s = _socket(domain, type, protocol);
-    return _open_osfhandle((intptr_t)s, O_RDWR);
+    return (int)s;
 }
