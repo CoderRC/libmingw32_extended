39b70203f75104fecbdc6515d97dca6ab7f927b6
["Ilker":{...}] <xentsl3pt3r@outlook.com>
2025-02-03 17:42:26 -0500

    fix: ws2tcpip net functions

--- include/netdb.h
+++ include/netdb.h
@@ -1,114 +1,91 @@
 #ifndef _NETDB_H
 #define _NETDB_H
 
-#include <netinet/in.h>
-#include <sys/socket.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <inttypes.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
-#define EAI_AGAIN      -3  /* Temporary failure in name resolution */
-#define EAI_BADFLAGS   -1  /* Invalid value for ai_flags */
-#define EAI_FAIL       -4  /* Non-recoverable failure in name resolution */
-#define EAI_FAMILY     -6  /* ai_family not supported */
-#define EAI_MEMORY     -10 /* Memory allocation failure */
-#define EAI_NONAME     -2  /* Name or service not known */
-#define EAI_SERVICE    -8  /* Service name not supported for ai_socktype */
-#define EAI_SOCKTYPE   -7  /* ai_socktype not supported */
-#define EAI_SYSTEM     -11 /* System error returned in errno */
-#define EAI_OVERFLOW   -12 /* Argument buffer overflow */
-
+#define EAI_AGAIN       11002  // WSATRY_AGAIN
+#define EAI_BADFLAGS    10022  // WSAEINVAL
+#define EAI_FAIL        11003  // WSANO_RECOVERY
+#define EAI_FAMILY      10047  // WSAEAFNOSUPPORT
+#define EAI_MEMORY      10012  // WSA_NOT_ENOUGH_MEMORY
+#define EAI_NONAME      11001  // WSAHOST_NOT_FOUND
+#define EAI_SERVICE     10109  // WSATYPE_NOT_FOUND
+#define EAI_SOCKTYPE    10044  // WSAESOCKTNOSUPPORT
+#define EAI_SYSTEM      10006  // Handled via errno
+#define EAI_OVERFLOW    10014  // WSAEFAULT
 
 struct addrinfo {
-    int              ai_flags;      /* Input flags */
-    int              ai_family;     /* Protocol family for socket */
-    int              ai_socktype;   /* Socket type */
-    int              ai_protocol;   /* Protocol for socket */
-    socklen_t        ai_addrlen;    /* Length of socket address */
-    struct sockaddr *ai_addr;       /* Socket address for socket */
-    char            *ai_canonname;  /* Canonical name for service location */
-    struct addrinfo *ai_next;       /* Pointer to next in list */
+    int             ai_flags;      // Input flags
+    int             ai_family;     // AF_xxx
+    int             ai_socktype;   // SOCK_xxx
+    int             ai_protocol;   // 0 or IPPROTO_xxx
+    size_t          ai_addrlen;    // Length of ai_addr
+    char            *ai_canonname; // Canonical name
+    struct sockaddr *ai_addr;      // Binary address
+    struct addrinfo *ai_next;      // Next structure
 };
 
-#define AI_PASSIVE     0x0001 /* Socket address is intended for bind() */
-#define AI_CANONNAME   0x0002 /* Request canonical name */
-#define AI_NUMERICHOST 0x0004 /* Don't use name resolution */
-#define AI_NUMERICSERV 0x0008 /* Don't use service name resolution */
-#define AI_V4MAPPED    0x0010 /* IPv4 mapped addresses if no IPv6 */
-#define AI_ALL         0x0020 /* Return both IPv4 and IPv6 addresses */
-#define AI_ADDRCONFIG  0x0040 /* Return addresses for each configured protocol */
+#define AI_PASSIVE          0x0001  // Socket for bind()
+#define AI_CANONNAME        0x0002  // Return canonical name
+#define AI_NUMERICHOST      0x0004  // Disable name resolution
+#define AI_NUMERICSERV      0x0008  // Disable service resolution
+#define AI_ALL              0x0100  // Query both IPv4 and IPv6
+#define AI_ADDRCONFIG       0x0400  // Use configured addresses
+#define AI_V4MAPPED         0x0800  // Map IPv4 to IPv6
 
-#define NI_NOFQDN      0x0001 /* Only return nodename portion for local hosts */
-#define NI_NUMERICHOST 0x0002 /* Return numeric form of the host's address */
-#define NI_NAMEREQD    0x0004 /* Error if the host's name not found */
-#define NI_NUMERICSERV 0x0008 /* Return numeric form of the service */
-#define NI_DGRAM       0x0010 /* Service is datagram service */
-#define NI_MAXHOST     1025   /* Maximum host name length returned */
-#define NI_MAXSERV     32     /* Maximum service name length returned */
+#define NI_NOFQDN       0x01
+#define NI_NUMERICHOST  0x02
+#define NI_NAMEREQD     0x04
+#define NI_NUMERICSERV  0x08
+#define NI_DGRAM        0x10
+#define NI_MAXHOST      1025
+#define NI_MAXSERV      32
 
-#define IPPORT_RESERVED 1024  /* Highest reserved Internet port number */
-
-void             endhostent(void);
-void             endnetent(void);
-void             endprotoent(void);
-void             endservent(void);
-void             freeaddrinfo(struct addrinfo *);
-const char      *gai_strerror(int);
-int              getaddrinfo(const char *restrict, const char *restrict,
-                    const struct addrinfo *restrict,
-                    struct addrinfo **restrict);
-struct hostent  *gethostbyaddr(const void *, socklen_t, int);
-struct hostent  *gethostbyname(const char *);
-struct hostent  *gethostent(void);
-int              getnameinfo(const struct sockaddr *restrict, socklen_t,
-                    char *restrict, socklen_t, char *restrict,
-                    socklen_t, int);
-struct netent   *getnetbyaddr(uint32_t, int);
-struct netent   *getnetbyname(const char *);
-struct netent   *getnetent(void);
-struct protoent *getprotobyname(const char *);
-struct protoent *getprotobynumber(int);
-struct protoent *getprotoent(void);
-struct servent  *getservbyname(const char *, const char *);
-struct servent  *getservbyport(int, const char *);
-struct servent  *getservent(void);
-void             sethostent(int);
-void             setnetent(int);
-void             setprotoent(int);
-void             setservent(int);
+#define IPPORT_RESERVED 1024
 
 struct hostent {
-    char  *h_name;            /* Official name of host */
-    char **h_aliases;         /* Alias list */
-    int    h_addrtype;        /* Host address type */
-    int    h_length;          /* Length of address */
-    char **h_addr_list;       /* List of addresses */
+    char  *h_name;      // Official host name
+    char  **h_aliases;  // Alias list
+    short h_addrtype;   // Address type (AF_INET/AF_INET6)
+    short h_length;     // Address length
+    char  **h_addr_list; // Address list
 };
-#define h_addr h_addr_list[0] /* Address, for backward compatibility */
+#define h_addr h_addr_list[0]
 
 struct netent {
-    char  *n_name;            /* Official name of net */
-    char **n_aliases;         /* Alias list */
-    int    n_addrtype;        /* Net address type */
-    uint32_t n_net;           /* Network number */
+    char     *n_name;    // Official network name
+    char     **n_aliases; // Alias list
+    short    n_addrtype; // Address type (AF_INET)
+    uint32_t n_net;      // Network number (host byte order)
 };
 
 struct protoent {
-    char  *p_name;            /* Official protocol name */
-    char **p_aliases;         /* Alias list */
-    int    p_proto;           /* Protocol number */
+    char  *p_name;    // Official protocol name
+    char  **p_aliases; // Alias list
+    int    p_proto;   // Protocol number
 };
 
 struct servent {
-    char  *s_name;            /* Official service name */
-    char **s_aliases;         /* Alias list */
-    int    s_port;            /* Port number */
-    char  *s_proto;           /* Protocol to use */
+    char  *s_name;    // Official service name
+    char  **s_aliases; // Alias list
+    short s_port;     // Port number (network byte order)
+    char  *s_proto;   // Protocol name
 };
 
+void             freeaddrinfo(struct addrinfo*);
+int              getaddrinfo(const char*, const char*, const struct addrinfo*, struct addrinfo**);
+int              getnameinfo(const struct sockaddr*, socklen_t, char*, size_t, char*, size_t, int);
+const char      *gai_strerror(int);
+
+struct hostent  *gethostbyaddr(const char*, int, int);
+struct hostent  *gethostbyname(const char*);
+struct servent  *getservbyport(int, const char*);
 
 #ifdef __cplusplus
 }
