487ce849fc89f5d00a9ecdcc760b0445cd48a591
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2025-06-20 14:50:21 +0300

    feat: improve dlsym + make it thread safe

--- source/dlsym.c
+++ source/dlsym.c
@@ -1,74 +1,154 @@
 #include <dlfcn.h>
 #include <windows.h>
+#include <synchapi.h>
+
+static SRWLOCK g_imports_lock = SRWLOCK_INIT;
 
 static PIMAGE_NT_HEADERS
-ImageNtHeader (PVOID Base)
+ImageNtHeader (void *base)
 {
-  PIMAGE_DOS_HEADER imageDosHeader = Base;
-  return (PIMAGE_NT_HEADERS) ((char *) (imageDosHeader) +
-			      imageDosHeader->e_lfanew);
+  if (!base)
+    return NULL;
+
+  PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER) base;
+  if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE)
+    return NULL;
+
+  PIMAGE_NT_HEADERS ntHeaders =
+    (PIMAGE_NT_HEADERS) ((BYTE *) base + dosHeader->e_lfanew);
+  if (ntHeaders->Signature != IMAGE_NT_SIGNATURE)
+    return NULL;
+
+  return ntHeaders;
 }
+
+// TODO: make this use a thread-safe hash/map
+static BOOL
+HasFixedImports (HMODULE module)
+{
+  static HMODULE patchedModules[64];
+  static size_t count = 0;
+
+  AcquireSRWLockExclusive (&g_imports_lock);
+
+  for (size_t i = 0; i < count; i++)
+    {
+      if (patchedModules[i] == module)
+	{
+	  ReleaseSRWLockExclusive (&g_imports_lock);
+	  return TRUE;
+	}
+    }
+
+  if (count < sizeof (patchedModules) / sizeof (patchedModules[0]))
+    patchedModules[count++] = module;
+
+  ReleaseSRWLockExclusive (&g_imports_lock);
+  return FALSE;
+}
+
+static inline void
+PatchIATEntry (PIMAGE_THUNK_DATA thunk, FARPROC procAddress)
+{
+#ifdef _WIN64
+  thunk->u1.Function = (ULONGLONG) procAddress;
+#else
+  thunk->u1.Function = (DWORD) procAddress;
+#endif
+}
+
+typedef BOOL (WINAPI * DllEntryProc) (HINSTANCE, DWORD, LPVOID);
 
 void *
 dlsym (void *__restrict __handle, const char *__restrict __name)
 {
-  PIMAGE_NT_HEADERS imageNtHeaders = ImageNtHeader (__handle);
-  MEMORY_BASIC_INFORMATION mbi;
-  VirtualQuery ((char *) (imageNtHeaders) +
-		imageNtHeaders->OptionalHeader.
-		DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress,
-		&mbi, sizeof (mbi));
-  if (mbi.Protect & PAGE_WRITECOPY)
+  if (!__handle || !__name)
     {
-      PIMAGE_IMPORT_DESCRIPTOR imageImportDescriptor;
-      imageImportDescriptor =
-	(PIMAGE_IMPORT_DESCRIPTOR) ((char *) (__handle) +
-				    imageNtHeaders->OptionalHeader.
-				    DataDirectory
-				    [IMAGE_DIRECTORY_ENTRY_IMPORT].
-				    VirtualAddress);
-      LPCSTR libFileName;
-      HMODULE hModule;
-      LPCSTR procName;
-      PIMAGE_THUNK_DATA imageThunkData;
-      while (imageImportDescriptor->Name != 0)
-	{
-	  libFileName = (char *) __handle + imageImportDescriptor->Name;
-	  hModule = LoadLibraryA (libFileName);
-	  if (hModule)
-	    {
-	      imageThunkData =
-		(PIMAGE_THUNK_DATA) ((char *) (__handle) +
-				     imageImportDescriptor->FirstThunk);
-	      while (imageThunkData->u1.AddressOfData != 0)
-		{
-		  if (IMAGE_SNAP_BY_ORDINAL (imageThunkData->u1.Ordinal))
-		    {
-		      procName =
-			(LPCSTR) IMAGE_ORDINAL (imageThunkData->u1.Ordinal);
-		    }
-		  else
-		    {
-		      procName =
-			((PIMAGE_IMPORT_BY_NAME)
-			 ((char *) (__handle) +
-			  imageThunkData->u1.AddressOfData))->Name;
-		    }
-		  imageThunkData->u1.Function =
-		    (DWORD_PTR) GetProcAddress (hModule, procName);
-		  ++imageThunkData;
-		}
-	    }
-	  imageImportDescriptor++;
-	}
-      BOOL WINAPI (*DllEntry) (HINSTANCE hinstDLL, DWORD fdwReason,
-			       LPVOID lpvReserved) =
-	(BOOL
-	 WINAPI (*)(HINSTANCE hinstDLL, DWORD fdwReason,
-		    LPVOID lpvReserved)) ((char *) (__handle) +
-					  imageNtHeaders->OptionalHeader.
-					  AddressOfEntryPoint);
-      (*DllEntry) ((HINSTANCE) __handle, DLL_PROCESS_ATTACH, 0);
+      SetLastError (ERROR_INVALID_PARAMETER);
+      return NULL;
     }
-  return GetProcAddress (__handle, __name);
+
+  HMODULE module = (HMODULE) __handle;
+
+  PIMAGE_NT_HEADERS ntHeaders = ImageNtHeader (__handle);
+  if (!ntHeaders)
+    {
+      return NULL;
+    }
+
+  IMAGE_DATA_DIRECTORY importDir =
+    ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+  if (importDir.VirtualAddress == 0 || importDir.Size == 0)
+    return NULL;
+
+  BYTE *baseAddr = (BYTE *) __handle;
+  PIMAGE_IMPORT_DESCRIPTOR importDesc =
+    (PIMAGE_IMPORT_DESCRIPTOR) (baseAddr + importDir.VirtualAddress);
+
+  MEMORY_BASIC_INFORMATION mbi;
+  if (VirtualQuery (importDesc, &mbi, sizeof (mbi)) == 0)
+    return NULL;
+
+  if ((mbi.Protect & PAGE_WRITECOPY) && !HasFixedImports (module))
+    {
+      for (; importDesc->Name != 0; importDesc++)
+	{
+	  LPCSTR libName = (LPCSTR) (baseAddr + importDesc->Name);
+	  HMODULE hModule = LoadLibraryA (libName);
+	  if (!hModule)
+	    continue;
+
+	  PIMAGE_THUNK_DATA importLookupTable =
+	    (PIMAGE_THUNK_DATA) (baseAddr + importDesc->OriginalFirstThunk);
+	  PIMAGE_THUNK_DATA importAddressTable =
+	    (PIMAGE_THUNK_DATA) (baseAddr + importDesc->FirstThunk);
+
+	  if (!importLookupTable)
+	    importLookupTable = importAddressTable;
+
+	  for (; importLookupTable->u1.AddressOfData != 0;
+	       importLookupTable++, importAddressTable++)
+	    {
+	      FARPROC procAddress = NULL;
+
+	      if (importLookupTable->u1.Ordinal & IMAGE_ORDINAL_FLAG)
+		{
+		  WORD ordinal =
+		    (WORD) (importLookupTable->u1.Ordinal & 0xFFFF);
+		  procAddress =
+		    GetProcAddress (hModule, (LPCSTR) (uintptr_t) ordinal);
+		}
+	      else
+		{
+		  PIMAGE_IMPORT_BY_NAME importByName =
+		    (PIMAGE_IMPORT_BY_NAME) (baseAddr +
+					     importLookupTable->u1.
+					     AddressOfData);
+		  procAddress = GetProcAddress (hModule, importByName->Name);
+		}
+
+	      if (!procAddress)
+		{
+		  procAddress = NULL;
+		}
+
+	      PatchIATEntry (importAddressTable, procAddress);
+	    }
+	}
+
+      FARPROC entryPoint =
+	(FARPROC) (baseAddr + ntHeaders->OptionalHeader.AddressOfEntryPoint);
+      if (entryPoint)
+	{
+	  DllEntryProc DllEntry = (DllEntryProc) entryPoint;
+
+	  if (!DllEntry ((HINSTANCE) __handle, DLL_PROCESS_ATTACH, NULL))
+	    {
+	      SetLastError (ERROR_DLL_INIT_FAILED);
+	      return NULL;
+	    }
+	}
+    }
+
+  return (LPVOID) GetProcAddress (module, __name);
 }
