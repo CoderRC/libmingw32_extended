0024f3628d9890bbc0265b6d57cec4e44a7c9cc1
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2025-06-20 14:58:42 +0300

    feat: cleanup dlerror + make dlerror_buffer thread local

--- source/dlerror.c
+++ source/dlerror.c
@@ -1,7 +1,9 @@
 #include <windows.h>
 #include <stdio.h>
 
-static char dlerror_buffer[512] = { 0 };
+static
+__declspec (thread)
+     char dlerror_buffer[512] = { 0 };
 
 char *
 dlerror (void)
@@ -12,11 +14,32 @@ dlerror (void)
       return NULL;
     }
 
-  FormatMessageA (FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		  NULL,
-		  err,
-		  MAKELANGID (LANG_NEUTRAL, SUBLANG_DEFAULT),
-		  dlerror_buffer, sizeof (dlerror_buffer), NULL);
+  DWORD flags = FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
+
+  DWORD length = FormatMessageA (flags,
+				 NULL,
+				 err,
+				 MAKELANGID (LANG_NEUTRAL, SUBLANG_DEFAULT),
+				 dlerror_buffer,
+				 sizeof (dlerror_buffer),
+				 NULL);
+
+  if (length == 0)
+    {
+      // Failed to format the message, fallback to numeric error code string
+      snprintf (dlerror_buffer, sizeof (dlerror_buffer),
+		"Unknown error 0x%08lx", err);
+    }
+  else
+    {
+      // Trim trailing newline characters (CRLF)
+      while (length > 0
+	     && (dlerror_buffer[length - 1] == '\n'
+		 || dlerror_buffer[length - 1] == '\r'))
+	{
+	  dlerror_buffer[--length] = '\0';
+	}
+    }
 
   SetLastError (0);
 
