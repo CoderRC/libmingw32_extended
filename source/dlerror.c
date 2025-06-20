3424bc7fba257dcd292ff61051aa78ee34a1655f
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2025-06-20 14:33:09 +0300

    feat: implement dlerror

--- source/dlerror.c
+++ source/dlerror.c
@@ -1,5 +1,24 @@
+#include <windows.h>
+#include <stdio.h>
+
+static char dlerror_buffer[512] = { 0 };
+
 char *
 dlerror (void)
 {
-  return 0;
+  DWORD err = GetLastError ();
+  if (err == 0)
+    {
+      return NULL;
+    }
+
+  FormatMessageA (FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		  NULL,
+		  err,
+		  MAKELANGID (LANG_NEUTRAL, SUBLANG_DEFAULT),
+		  dlerror_buffer, sizeof (dlerror_buffer), NULL);
+
+  SetLastError (0);
+
+  return dlerror_buffer;
 }
